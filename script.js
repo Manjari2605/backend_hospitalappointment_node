53588ea030c301502ac40628d3e77fdbc76df0ee
Manjari Kalisetty <manjari_kalisetty@srmap.edu.in>
2026-06-18 11:32:17 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const app=express()
-const users=[{id:1, name:"vijay"},
-    {id:2,name:"suresh"}
-]
-app.get("/",(roq,res)=>{
-    res.status(200).send("<h2>Manjari</h2>")
-})
